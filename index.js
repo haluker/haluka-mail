34409eac6ca4a79d2e9f590d787847aea213ba55
Robin Panta <hacktivistic@gmail.com>
2022-07-01 13:22:54 +0530

    Haluka Mail Provider

---
+++ index.js
@@ -0,0 +1,15 @@
+
+const { ServiceProvider } = require('@haluka/core')
+const nodemailer = require('nodemailer')
+
+exports.default = class MailServiceProvider extends ServiceProvider {
+	register () {
+		this.app.singleton('Haluka/Provider/Mail', function (app, { NodeMailerConfig } ) {
+			let transporter = nodemailer.createTransport(NodeMailerConfig)
+
+			if (NodeMailerConfig.verifyOnStart) {
+				transporter.verify(NodeMailerConfig.onVerificationError || (() => {}))
+			}
+		})
+	}
+}
